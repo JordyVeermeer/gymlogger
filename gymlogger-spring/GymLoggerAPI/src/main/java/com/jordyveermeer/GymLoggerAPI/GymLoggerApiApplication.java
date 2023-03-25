2e192a09875105fad2db89dee0c008d0c3ea0934
JordyVeermeer <jordy.veermeer@gmail.com>
2023-03-25 02:13:16 +0100

    added spring template

---
+++ gymlogger-spring/GymLoggerAPI/src/main/java/com/jordyveermeer/GymLoggerAPI/GymLoggerApiApplication.java
@@ -0,0 +1,13 @@
+package com.jordyveermeer.GymLoggerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymLoggerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymLoggerApiApplication.class, args);
+	}
+
+}
---
+++ gymlogger-spring/GymLoggerAPI/src/test/java/com/jordyveermeer/GymLoggerAPI/GymLoggerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordyveermeer.GymLoggerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymLoggerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
