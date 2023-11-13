e381a88a938ae2797b9ddbf902c5a188544298e5
JordyVeermeer <jordy.veermeer@gmail.com>
2023-11-13 23:48:37 +0100

    start dashboard mobile

--- gymlogger-react/src/components/dashboard/Dashboard.jsx
+++ gymlogger-react/src/components/dashboard/Dashboard.jsx
@@ -1,7 +1,25 @@
+import { Link } from 'react-router-dom';
+import '../dashboard/dashboard.css';
+import imgDashboard from '../../assets/dashboard_logo.png';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+
 
 export const Dashboard = () => {
 
     return (
-        <h1>This is your dashboard!</h1>
+        <div className="dashboard">
+            <div className="dashboard-top-section">
+                <div className="top-section-left">
+                    <h1>Schedule your workout program now.</h1>
+                    <Link className='actionbutton-dashboard' to={'/schedules'} >
+                        <span>start now</span>
+                        <ArrowCircleRightIcon sx={{ color: '#b3ba45' }} />
+                    </Link>
+                </div>
+                <div className="top-section-right">
+                    <img src={imgDashboard} alt="Logo" />
+                </div>
+            </div>
+        </div>
     );
 };
